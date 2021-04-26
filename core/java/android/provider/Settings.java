a7b60d88028e791047bc0d171a040143acf78c42
Stepan Kazakov <ksrt12group@gmail.com>
2021-04-26 22:25:14 +0300

    Revert "Revert "SystemUI: Old style mobile data indicators [1/2]""
    This reverts commit d07c0bef14f59eba638dbf311481866da088b981.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5906,6 +5906,11 @@ public final class Settings {
         public static final String SENSOR_BLOCKED_APP_DUMMY = "sensor_blocked_app_dummy";
 
         /**
+         * @hide
+         */
+        public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";
+
+        /**
          * Use doubletap as doze pulse triggers
          * @hide
          */
