2c8e5b76f918d991b477f2619f7ea0d93e12cb70
ksrt12 <ksrt12group@gmail.com>
2021-05-19 10:49:53 +0300

    Revert "SystemUI: Hide 4G+/LTE+"
    This reverts commit 2e5b78b7cfda96cf8e91e2a1ed1b15098385564e.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -1307,7 +1307,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
         boolean show4gFor3g = false;
         boolean alwaysShowCdmaRssi = false;
         boolean show4gForLte = false;
-        boolean hideLtePlus = true;
+        boolean hideLtePlus = false;
         boolean hspaDataDistinguishable;
         boolean inflateSignalStrengths = false;
         boolean alwaysShowDataRatIcon = false;
@@ -1339,6 +1339,8 @@ public class NetworkControllerImpl extends BroadcastReceiver
                         CarrierConfigManager.KEY_SHOW_4G_FOR_LTE_DATA_ICON_BOOL);
                 config.show4gFor3g = b.getBoolean(
                         CarrierConfigManager.KEY_SHOW_4G_FOR_3G_DATA_ICON_BOOL);
+              /*config.hideLtePlus = b.getBoolean(
+                        CarrierConfigManager.KEY_HIDE_LTE_PLUS_DATA_ICON_BOOL);*/
             }
 
             config.showVolteIcon = res.getBoolean(R.bool.config_display_volte);
